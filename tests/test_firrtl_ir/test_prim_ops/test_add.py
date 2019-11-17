5899973dc61b14e73923094866c7ac934c3bd5db
Gaufoo <zhongzc_arch@outlook.com>
2019-11-18 01:18:16 +0800

    test_firrtl_ir: refactor

--- tests/test_firrtl_ir/test_prim_ops/test_add.py
+++ tests/test_firrtl_ir/test_prim_ops/test_add.py
@@ -1,6 +1,7 @@
 from py_hcl.firrtl_ir.expr.prim_ops import Add
 from py_hcl.firrtl_ir.shortcuts import uw, sw
-from py_hcl.firrtl_ir.type import UIntType, SIntType, VectorType, BundleType
+from py_hcl.firrtl_ir.type import UIntType, SIntType, VectorType, \
+    BundleType, UnknownType
 from .helper import OpCase, basis_tester, encounter_error_tester
 
 
@@ -8,76 +9,52 @@ def max_width(x, y):
     return max(x.tpe.width.width, y.tpe.width.width)
 
 
-add_basis_cases = [
-    OpCase(
-        Add
-    ).arg_types(
-        UIntType, UIntType
-    ).res_type(
-        lambda x, y: uw(max_width(x, y) + 1)
-    ),
+def args(*arg_types):
+    class C:
+        @staticmethod
+        def tpe(res_type):
+            return OpCase(Add).arg_types(*arg_types).res_type(res_type)
 
-    OpCase(
-        Add
-    ).arg_types(
-        SIntType, SIntType
-    ).res_type(
-        lambda x, y: sw(max_width(x, y) + 1)
-    ),
+    return C
+
+
+add_basis_cases = [
+    args(UIntType, UIntType).tpe(lambda x, y: uw(max_width(x, y) + 1)),
+    args(SIntType, SIntType).tpe(lambda x, y: sw(max_width(x, y) + 1)),
 ]
 
 add_type_wrong_cases = [
-    OpCase(
-        Add
-    ).arg_types(
-        UIntType, VectorType
-    ).res_type(
-        lambda x, y: uw(5)
-    ),
-
-    OpCase(
-        Add
-    ).arg_types(
-        UIntType, BundleType
-    ).res_type(
-        lambda x, y: uw(5)
-    ),
-
-    OpCase(
-        Add
-    ).arg_types(
-        SIntType, VectorType
-    ).res_type(
-        lambda x, y: sw(5)
-    ),
-
-    OpCase(
-        Add
-    ).arg_types(
-        SIntType, BundleType
-    ).res_type(
-        lambda x, y: sw(5)
-    ),
+    args(UnknownType, UnknownType).tpe(lambda x, y: uw(32)),
+    args(UnknownType, UIntType).tpe(lambda x, y: uw(32)),
+    args(UnknownType, SIntType).tpe(lambda x, y: sw(32)),
+    args(UnknownType, VectorType).tpe(lambda x, y: sw(32)),
+    args(UIntType, VectorType).tpe(lambda x, y: uw(32)),
+    args(UIntType, BundleType).tpe(lambda x, y: uw(32)),
+    args(UIntType, UnknownType).tpe(lambda x, y: uw(32)),
+    args(SIntType, VectorType).tpe(lambda x, y: sw(32)),
+    args(SIntType, BundleType).tpe(lambda x, y: sw(32)),
+    args(SIntType, UnknownType).tpe(lambda x, y: sw(32)),
+    args(VectorType, UIntType).tpe(lambda x, y: uw(32)),
+    args(VectorType, SIntType).tpe(lambda x, y: sw(32)),
+    args(VectorType, VectorType).tpe(lambda x, y: uw(32)),
+    args(VectorType, BundleType).tpe(lambda x, y: uw(32)),
+    args(BundleType, UIntType).tpe(lambda x, y: uw(32)),
+    args(BundleType, SIntType).tpe(lambda x, y: uw(32)),
+    args(BundleType, VectorType).tpe(lambda x, y: uw(32)),
+    args(BundleType, UnknownType).tpe(lambda x, y: uw(32)),
+    args(BundleType, BundleType).tpe(lambda x, y: uw(32)),
 ]
 
 add_width_wrong_cases = [
-    OpCase(
-        Add
-    ).arg_types(
-        UIntType, UIntType
-    ).res_type(
-        lambda x, y: uw(max_width(x, y))
-    ),
-
-    OpCase(
-        Add
-    ).arg_types(
-        SIntType, SIntType
-    ).res_type(
-        lambda x, y: sw(max_width(x, y))
-    ),
+    args(UIntType, UIntType).tpe(lambda x, y: uw(max_width(x, y))),
+    args(SIntType, SIntType).tpe(lambda x, y: sw(max_width(x, y))),
+    args(UIntType, UIntType).tpe(lambda x, y: uw(max_width(x, y) - 1)),
+    args(SIntType, SIntType).tpe(lambda x, y: sw(max_width(x, y) - 1)),
+    args(SIntType, SIntType).tpe(lambda x, y: sw(1)),
 ]
 
-basis_tester(add_basis_cases)
-encounter_error_tester(add_type_wrong_cases)
-encounter_error_tester(add_width_wrong_cases)
+
+def test_add():
+    basis_tester(add_basis_cases)
+    encounter_error_tester(add_type_wrong_cases)
+    encounter_error_tester(add_width_wrong_cases)
