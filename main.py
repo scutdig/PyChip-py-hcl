30125ff6c773ebd962159885d0528883a1b0de8e
201830600181@mail.scut.edu.cn <5716689+msk233@user.noreply.gitee.com>
2021-12-18 23:10:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,35 @@
 from pyhcl import *
 from pysv import sv, DataType, Reference
-from pyhcl.simulator import DpiConfig, Simulator
+from pyhcl.simulator import Simlite, DpiConfig
 
-class Xor(BlackBox):
+class Add(BlackBox):
     io = IO(
-        in1=Input(U.w(1)),
-        in2=Input(U.w(1)),
-        out=Output(U.w(1))
+        in1=Input(U.w(32)),
+        in2=Input(U.w(32)),
+        out=Output(U.w(32))
     )
 
-@sv(a=DataType.Bit, b=DataType.Bit, return_type=Reference(x = DataType.Bit))
-def fn(a, b):
-    return a ^ b
 
-addpysvmodule(Xor, fn)
+@sv(a=DataType.UInt, b=DataType.UInt, return_type=Reference(x = DataType.UInt))
+def fn(a, b):
+    return a + b
+
+addpysvmodule(Add, fn)
 compile_and_binding_all()
+
 
 
 class Top(Module):
     io = IO(
-        a=Input(U.w(1)),
-        b=Input(U.w(1)),
-        c=Output(U.w(1))
+        a=Input(U.w(32)),
+        b=Input(U.w(32)),
+        c=Output(U.w(32))
     )
 
-    xor = Xor()
-    xor.io.in1 <<= io.a
-    xor.io.in2 <<= io.b
-    io.c <<= xor.io.out
+    add = Add()
+    add.io.in1 <<= io.a
+    add.io.in2 <<= io.b
+    io.c <<= add.io.out
 
 
 from random import randint
@@ -36,18 +38,8 @@ if __name__ == '__main__':
     cfg = DpiConfig()
     #Emitter.dumpVerilog(Emitter.dump(Emitter.emit(Top()), "Top.fir"))
 
-    s = Simulator(Top(),cfg)
-    
-    handler = s.handler
-
-    # ---------- Simulation begin ---------- #
-    s.start()
-
-    for i in range(16):
-        s.poke(handler.io.a, randint()&1)
-        s.poke(handler.io.b, randint()&1)
-        s.step()
-        s.peek(handler.io.c)
-    s.term()
-    # ---------- Simulation end ---------- #
-    
+    s = Simlite(Top(), cfg)
+    s.step([20, 20])
+    s.step([15, 10])
+    s.step([1000, 1])
+    s.step([999, 201])
