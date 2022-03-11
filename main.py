0c3c0011576c8ebf2de87a1f890d51b73eeb6ac4
raybdzhou <bdzhou1999@gmail.com>
2022-03-11 18:18:46 +0800

    feat: add high form checking pass.

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from pyhcl import *
 
-
 class MOD(Module):
     io = IO(
         a=Input(U.w(32)),
@@ -21,13 +20,82 @@ class MOD(Module):
 
     io.z <<= x
 
+class FullAdder(Module):
+    io = IO(
+        a=Input(U.w(1)),
+        b=Input(U.w(1)),
+        cin=Input(U.w(1)),
+        s=Output(U.w(1)),
+        cout=Output(U.w(1))
+    )
 
-def main():
-    f = Emitter.dump(Emitter.emit(MOD()), "mod.v")
-    Emitter.dumpVerilog(f)
+    io.s <<= io.a ^ io.b ^ io.cin
+    io.cout <<= (io.a & io.b) | (io.a & io.cin) | (io.b & io.cin)
 
+
+class MemDemo(Module):
+    io = IO(
+        i=Input(U.w(16)),
+        o=Output(U.w(16)),
+    )
+
+    m = Mem(10, U.w(16))
+    m[U(2)] <<= io.i
+    io.o <<= m[U(2)]
+
+class MuxVec(Module):
+    io = IO(
+        i=Input(Bool),
+        o=Output(Vec(4, Vec(8, U.w(32)))),
+    )
+
+    a = VecInit(VecInit(U.w(32)(i) for i in range(8)) for _ in range(4))
+    b = VecInit(VecInit(U.w(32)(i) for i in range(7, -1, -1)) for _ in range(4))
+
+    io.o <<= Mux(io.i, a, b)
+
+class GCD(Module):
+    io = IO(
+        a=Input(U.w(32)),
+        b=Input(U.w(32)),
+        e=Input(Bool),
+        z=Output(U.w(32)),
+        v=Output(Bool),
+    )
+
+    x = Reg(U.w(32))
+    y = Reg(U.w(32))
+
+    with when(x > y):
+        x <<= x - y
+    with otherwise():
+        y <<= y - x
+
+    with when(io.e):
+        x <<= io.a
+        y <<= io.b
+
+    io.z <<= x
+    io.v <<= y == U(0)
+
+from pyhcl.ir.low_ir import NoInfo
+from pyhcl.passes.check_form import NotUniqueException
 
 if __name__ == '__main__':
-    main()
+    # Emitter.dumpVerilog(Emitter.dump(Emitter.emit(MuxVec()), "MuxVec.fir"), True)
+    Emitter.dump(Emitter.emit(MuxVec(), True), "MuxVec.v")
+    # Emitter.dumpLowForm(Emitter.dump(Emitter.emit(MuxVec()), "MuxVec.fir"), True)
+    
+
+
+
+
+# def main():
+#     f = Emitter.dump(Emitter.emit(MOD()), "mod.v")
+#     Emitter.dumpVerilog(f)
+
+
+# if __name__ == '__main__':
+#     main()
 
 
---
+++ pyhcl/passes/utils.py
@@ -0,0 +1,79 @@
+from pyhcl.ir.low_ir import *
+from pyhcl.passes.wir import Flow, SinkFlow, SourceFlow
+from typing import List
+from pyhcl.passes._pass import PassException
+
+def is_max(w1: Width, w2: Width):
+    return IntWidth(max(w1.width, w2.width))
+
+def to_flow(d: Direction) -> Flow:
+    if type(d) == Input:
+        return SourceFlow()
+    if type(d) == Output:
+        return SinkFlow()
+
+def flow(e: Expression) -> Flow:
+    if type(e) == DoPrim:
+        return SourceFlow()
+    elif type(e) == UIntLiteral:
+        return SourceFlow()
+    elif type(e) == SIntLiteral:
+        return SourceFlow()
+    elif type(e) == Mux:
+        return SourceFlow()
+    elif type(e) == ValidIf:
+        return SourceFlow()
+    else:
+        raise PassException(f'flow: shouldn\'t be here - {e}')
+
+def mux_type_and_widths(e1, e2) -> Type:
+    return mux_type_and_width(e1.typ, e2.typ)
+
+def mux_type_and_width(t1: Type, t2: Type) -> Type:    
+    if type(t1) == ClockType and type(t2) == ClockType:
+        return ClockType()
+    elif type(t1) == AsyncResetType and type(t2) == AsyncResetType:
+        return AsyncResetType()
+    elif type(t1) == UIntType and type(t2) == UIntType:
+        return UIntType(is_max(t1.width, t2.width))
+    elif type(t1) == VectorType and type(t2) == VectorType:
+        return VectorType(mux_type_and_width(t1.typ, t2.typ), t1.size)
+    elif type(t1) == BundleType and type(t2) == BundleType:
+        return BundleType(map(lambda f1, f2: Field(f1.name, f1.flip, mux_type_and_width(f1.typ, f2.typ)), list(zip(t1.fields, t2.fields))))
+    else:
+        return UnknownType
+
+def create_exps(e: Expression) -> List[Expression]:
+    if type(e) == Mux:
+        e1s = create_exps(e.tval)
+        e2s = create_exps(e.fval)
+        return list(map(lambda e1, e2: Mux(e.cond, e1, e2, mux_type_and_widths(e1, e2)), list(zip(e1s, e2s))))
+    elif type(e) == ValidIf:
+        return map(lambda e1: ValidIf(e.cond, e1, e1.typ), create_exps(e.value))
+    else:
+        if type(e.typ) == GroundType:
+            return list(e)
+        elif type(e.typ) == BundleType:
+            # TODO: add flatMap function
+            return list(e)
+        elif type(e.typ) == VectorType:
+            # TODO: add flatMap function
+            return list(e)
+
+def get_info(s: Statement) -> Info:
+    if hasattr(s, 'info'):
+        return s.info
+    else:
+        return NoInfo
+
+def has_flip(typ: Type) -> bool:
+    if type(typ) == BundleType:
+        for f in typ.fields:
+            if type(f) == Flip:
+                return True
+            else:
+                return has_flip(f.typ)
+    elif type(typ) == VectorType:
+        return has_flip(typ.typ)
+    else:
+        return False
