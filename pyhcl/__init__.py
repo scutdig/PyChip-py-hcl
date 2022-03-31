56d469d08ed6db7eaf03b119b3fdc9f7b9810fe5
Msk <201830600181@mail.scut.edu.cn>
2022-03-31 19:01:37 +0800

    Qemu : Add qemutool and example

--- pyhcl/__init__.py
+++ pyhcl/__init__.py
@@ -3,3 +3,4 @@ from .lib import *
 from .util.pysvtools import addpysvmodule, compile_and_binding_all
 from .util.firrtltools import addfirrtlmodule
 from .util.functions import with_debug_info
+from .util.qemutools import Qemu
