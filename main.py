92310f9a74e359bf5b5cc3e797cd3285232024b1
Msk <201830600181@mail.scut.edu.cn>
2022-05-08 18:04:34 +0800

    Add example of firrtltool

--- main.py
+++ main.py
@@ -1,33 +1,61 @@
 from pyhcl import *
 
+W = 8  # 位宽
 
-class MOD(Module):
-    io = IO(
-        a=Input(U.w(32)),
-        b=Input(U.w(32)),
-        e=Input(Bool),
-        z=Output(U.w(32)),
-    )
 
-    x = RegInit(U.w(32)(0))
-    y = RegInit(U.w(32)(0))
+def matrixMul(x: int, y: int, z: int):
+    """
+    x*y × y*z 矩阵乘法电路
+    """
 
-    with when(x >= y):
-        x <<= x - y
+    class MatrixMul(Module):
+        io = IO(
+            a=Input(Vec(x, Vec(y, U.w(W)))),
+            b=Input(Vec(y, Vec(z, U.w(W)))),
+            o=Output(Vec(x, Vec(z, U.w(W)))),
+        )
 
-    with when(io.e):
-        x <<= io.a
-        y <<= io.b
+        for i, a_row in enumerate(io.a):
+            for j, b_col in enumerate(zip(*io.b)):
+                io.o[i][j] <<= Sum(a * b for a, b in zip(a_row, b_col))
 
-    io.z <<= x
+    return MatrixMul()
+
+
+def bias(n):
+    return U.w(W)(n)
+
+
+def weight(lst):
+    return VecInit(U.w(W)(i) for i in lst)
+
+
+def neurons(w, b):
+    """
+    参数：权重向量 w，偏移量 b
+    输出：神经网络神经元电路  *注：暂无通过非线性传递函数
+    """
+
+    class Unit(Module):
+        io = IO(
+            i=Input(Vec(len(w), U.w(W))),
+            o=Output(U.w(W))
+        )
+        m = matrixMul(1, len(w), 1).io
+        m.a <<= io.i
+
+        m.b <<= w
+        io.o <<= m.o[0][0] + b
+
+    return Unit()
 
 
 def main():
-    f = Emitter.dump(Emitter.emit(MOD()), "mod.v")
-    Emitter.dumpVerilog(f)
+    # 得到权重向量为[3, 4, 5, 6, 7, 8, 9, 10]，偏移量为14的神经元电路
+    n = neurons(weight([3, 4, 5, 6, 7, 8, 9, 10]), bias(14))
+    f = Emitter.dump(Emitter.emit(n), "neurons.fir")
+    #Emitter.dumpVerilog(f)
 
 
 if __name__ == '__main__':
     main()
-
-
